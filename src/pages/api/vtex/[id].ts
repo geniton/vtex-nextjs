1e859fcfa110daaa0f94b1a2f71febbbb421b4fa
Geniton Lima <genitom.wd@gmail.com>
2023-08-01 10:33:58 -0300

    chore: remove cors

--- src/pages/api/vtex/[id].ts
+++ src/pages/api/vtex/[id].ts
@@ -5,6 +5,16 @@ import { Services } from '@retailhub/audacity-vtex'
 import storeConfig from 'store.config'
 
 const Vtex: NextApiHandler = async (request, response) => {
+  response.setHeader('Access-Control-Allow-Credentials', 'true')
+  response.setHeader('Access-Control-Allow-Origin', '*')
+  response.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET,OPTIONS,PATCH,DELETE,POST,PUT'
+  )
+  response.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
   try {
     return Services.Vtex.VtexApiHandler(request, response, {
       store: storeConfig.api.storeId,
